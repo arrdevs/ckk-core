6017a3057b9cb89ddb7e29d21d1f1dd3ea3154fb
arrizaqu@yahoo.com <arrizaqu@yahoo.com>
2018-11-25 10:04:14 +0700

    add user login -getuser and save user

--- src/main/java/com/keranjangkita/controller/api/LoginApiController.java
+++ src/main/java/com/keranjangkita/controller/api/LoginApiController.java
@@ -2,8 +2,10 @@ package com.keranjangkita.controller.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.keranjangkita.model.User;
 import com.keranjangkita.service.LoginService;
@@ -16,6 +18,7 @@ public class LoginApiController {
 	LoginService loginService;
 	
 	@RequestMapping(value="/login")
+	@ResponseBody
 	public User getUserApp(@RequestParam("username") String username,@RequestParam("password") String password) {
 		User user = loginService.getUsernameByUsernameAndPassword(username, password);
 		return user;
